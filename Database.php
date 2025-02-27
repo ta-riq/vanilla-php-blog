8474ddd6fa3a09f76659f27f3c43843606d48bc1
Tariqul Islam <leg3ndtariq@gmail.com>
2025-02-27 17:23:57 +0600

    updated the MYSQL QUERY fetch Mode.

--- Database.php
+++ Database.php
@@ -3,10 +3,12 @@
 class Database {
     
     public $connection;
-    
+
     public function __construct() {
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset";
-        $this->connection = new PDO($dsn);
+        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+        $this->connection = new PDO($dsn, "root", "", [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
 
     public function query($query) {
--- index.php
+++ index.php
@@ -4,6 +4,6 @@ require "Database.php";
 
 $db = new Database();
 
-$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
+$posts = $db->query("SELECT * FROM posts")->fetchAll();
 
 require "routes.php";
